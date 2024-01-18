1a073eeaf688028d7c62a75150cfb4d497ee52e5
전성호 <kopasd99@gmail.com>
2024-01-18 19:06:00 +0800

    add container on pure Java

--- src/main/java/sungho1/springCorePrinciple/AppConfig.java
+++ src/main/java/sungho1/springCorePrinciple/AppConfig.java
@@ -1,5 +1,7 @@
 package sungho1.springCorePrinciple;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import sungho1.springCorePrinciple.discount.DiscountPolicy;
 import sungho1.springCorePrinciple.discount.FixDiscountPolicy;
 import sungho1.springCorePrinciple.discount.RateDiscountPolicy;
@@ -10,20 +12,24 @@ import sungho1.springCorePrinciple.member.MemoryMemberRepository;
 import sungho1.springCorePrinciple.order.OrderService;
 import sungho1.springCorePrinciple.order.OrderServiceImpl;
 
+@Configuration
 public class AppConfig {
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(
                 memberRepository(),
                 discountPolicy()
         );
     }
-
+    @Bean
     public MemberRepository memberRepository(){
         return new MemoryMemberRepository();
     }
+    @Bean
     public DiscountPolicy discountPolicy(){
         return new RateDiscountPolicy();
     }
--- src/main/java/sungho1/springCorePrinciple/MemberApp.java
+++ src/main/java/sungho1/springCorePrinciple/MemberApp.java
@@ -1,5 +1,7 @@
 package sungho1.springCorePrinciple;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import sungho1.springCorePrinciple.member.Grade;
 import sungho1.springCorePrinciple.member.Member;
 import sungho1.springCorePrinciple.member.MemberService;
@@ -7,8 +9,11 @@ import sungho1.springCorePrinciple.member.MemberServiceImpl;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig=new AppConfig();
-        MemberService memberService= appConfig.memberService();
+        //AppConfig appConfig=new AppConfig();
+        //MemberService memberService= appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService",MemberService.class);
 
         Member memberA = new Member(1L, "MemberA", Grade.VIP);
         memberService.join(memberA);
